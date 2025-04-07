59fd6172148cffdfb7a3828d24ff93fe481393f2
Juan Galvis <Galvis>
2025-04-07 08:43:24 -0500

    Include Test Class and test result

---
+++ Java/Oracle/OCA/Exercises/Example4/TestClass.java
@@ -0,0 +1,8 @@
+package Java.Oracle.OCA.Exercises.Example4;
+
+public class TestClass {
+    public static void main(String[] args) {
+        House ciHouse = new MyHouse();
+        System.out.println(ciHouse.getAddress());
+    }
+}
