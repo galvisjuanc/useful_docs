8e0353245dc60c4e0ebdde7165fc366c4ee3fac3
Juan Galvis <Galvis>
2025-04-09 11:04:45 -0500

    Working with Inheritance I

---
+++ Java/Oracle/OCA/Exercises/Example9/TestClassEx.java
@@ -0,0 +1,14 @@
+package Java.Oracle.OCA.Exercises.Example9;
+
+interface Flyer {
+    String getName();
+}
+
+
+public class TestClassEx {
+
+    public static void main(String[] args) {
+        //Flyer f = new Eagle("American Bald Eagle");
+    }
+    
+}
