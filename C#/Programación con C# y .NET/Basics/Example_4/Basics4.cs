5cd0779544b87742029117b967847c6bb7018059
Juan Galvis <Galvis>
2025-04-10 11:03:56 -0500

    Tweak indentation

--- C#/Programación con C# y .NET/Basics/Example_4/Basics4.cs
+++ C#/Programación con C# y .NET/Basics/Example_4/Basics4.cs
@@ -1,21 +1,21 @@
 namespace Example_4
 {
     internal class Basics4
-{
-    static void Main(string[] args)
     {
-        var radio = 0.0;
-        var resultado = 0d;
-        const double Pi = 3.141591;
+        static void Main(string[] args)
+        {
+            var radio = 0.0;
+            var resultado = 0d;
+            const double Pi = 3.141591;
 
-        Console.WriteLine("Bienvenido, Calculemos circulos");
-        Console.WriteLine("Ingrese el valor del radio: ");
+            Console.WriteLine("Bienvenido, Calculemos circulos");
+            Console.WriteLine("Ingrese el valor del radio: ");
 
-        radio = Convert.ToDouble(Console.ReadLine());
+            radio = Convert.ToDouble(Console.ReadLine());
 
-        resultado = Math.PI * Math.Pow(radio,2);
+            resultado = Math.PI * Math.Pow(radio,2);
 
-        Console.WriteLine("El área de tu circulo es de: " + resultado);
+            Console.WriteLine("El área de tu circulo es de: " + resultado);
+        }
     }
-}
 }
