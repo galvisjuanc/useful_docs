60fc47a6c321d2a638c6d0eedede466b1d6cf11a
Juan Galvis <Galvis>
2025-04-11 10:04:05 -0500

    Add static variables for this Real Brainer Exercise

---
+++ Java/Oracle/OCA/Exercises/Example11/TestClassExample.java
@@ -0,0 +1,14 @@
+package Java.Oracle.OCA.Exercises.Example11;
+
+public class TestClassExample {
+
+    static boolean b;
+    static int[] ia = new int[1];
+    static char ch;
+    static boolean[] ba = new boolean[1];
+
+    public static void main(String[] args) {
+        
+    }
+
+}
