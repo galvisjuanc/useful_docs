76a0f04a25784bd53f3365157b948a79dbdd3015
Juan Camilo Galvis Cuellar <Galvis>
2025-04-19 12:37:40 -0500

    Add the exercise

--- Java/Oracle/OCA/Example16/TestClass.java
+++ Java/Oracle/OCA/Example16/TestClass.java
@@ -1,3 +1,11 @@
+package Java.Oracle.OCA.Example16;
+
 public class TestClass {
-    
+    public static void main(String[] args) throws Exception{
+        int i = 1, j = 10;
+        do {
+           if (i++ > --j) continue;
+        } while (i < 5);
+        System.out.println("i=" + i + " j=" + j);
+    }
 }
