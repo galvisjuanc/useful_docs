fea73515bdccbf0f6314941795ec79edc60e5aad
Juan Camilo Galvis Cuellar <Galvis>
2025-04-22 10:06:10 -0500

    Include properties for the SuperHero class

---
+++ C#/Programación con C# y .NET/POO con C#/POO1/SuperHeroe.cs
@@ -0,0 +1,12 @@
+namespace POO1
+{
+    class SuperHeroe
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string IdentidadSecreta { get; set; }
+        public string Ciudad { get; set; }
+        public List<SuperPoder> SuperPoderes { get; set; }
+        public bool PuedeVolar { get; set; }
+    }
+}
