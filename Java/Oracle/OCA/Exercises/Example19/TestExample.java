cba15f1706fd545584a703e69e64af90e4d4eacd
Juan Camilo Galvis Cuellar <Galvis>
2025-04-22 10:41:05 -0500

    Switch used with a boolean

---
+++ Java/Oracle/OCA/Exercises/Example19/TestExample.java
@@ -0,0 +1,11 @@
+package Java.Oracle.OCA.Exercises.Example19;
+
+public class TestExample {
+    public static void main(String[] args) {
+        boolean flag = true;
+        /*switch (flag) {
+            case true: System.out.println("true");
+            default: System.out.println("false");
+        }*/
+    }
+}
