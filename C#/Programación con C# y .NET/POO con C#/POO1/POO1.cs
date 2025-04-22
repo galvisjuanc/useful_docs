8d714bbb9b28233827b082bd45d7380208df77d7
Juan Camilo Galvis Cuellar <Galvis>
2025-04-22 10:26:13 -0500

    Add a new superpower

--- C#/Programación con C# y .NET/POO con C#/POO1/POO1.cs
+++ C#/Programación con C# y .NET/POO con C#/POO1/POO1.cs
@@ -5,6 +5,11 @@ namespace POO1
         static void Main(string[] args)
         {
             SuperHeroe superHeroe = new SuperHeroe();
+
+            var poderVolar = new SuperPoder();
+            poderVolar.Nombre = "Volar";
+            poderVolar.Descripcion = "Capacidad para volar y planear en el aire";
+            poderVolar.Nivel = NivelPoder.NivelDos;
         }    
     }
 }
