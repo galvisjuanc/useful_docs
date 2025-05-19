320f763156806ad34987a10f082a15c8ae9aa00b
Juan Galvis <Galvis>
2025-05-19 08:32:49 -0500

    Explaining the code changes

--- Java/Oracle/OCA/Exercises/Example24/Test1.java
+++ Java/Oracle/OCA/Exercises/Example24/Test1.java
@@ -7,9 +7,9 @@ import java.util.Set;
 class Test1 {
     public static void main(String[] args) {
         Set<String> set = new HashSet<>(Arrays.asList("one", "two", "three"));
-        set.add("one");
+        set.add("one"); //this is a duplicated value. It will not be stored on the set.
         set.add(null);
-        set.add(null);
+        set.add(null); // same values can't be added to a set. A set doesn't accept duplicated values
         System.out.println(set.size());
         for (String value : set) {
             System.out.println(value + " ");
