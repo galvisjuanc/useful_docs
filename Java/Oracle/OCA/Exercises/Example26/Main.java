f2f5b3926ea8c4f77cfc389c0e0ae1811dd50cfe
Juan Galvis <Galvis>
2025-05-22 09:56:47 -0500

    Verify arrays

---
+++ Java/Oracle/OCA/Exercises/Example26/Main.java
@@ -0,0 +1,10 @@
+package Java.Oracle.OCA.Exercises.Example26;
+
+class Main {
+    
+    public static void main(String[] args) {
+        int[] array1, array2[];
+        int[][] array3;
+        int[] array4[], array5[];    
+    }
+}
