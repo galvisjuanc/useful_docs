9964cac5de1b9a3b6db358d96afe729c181ba7b1
Juan Galvis <Galvis>
2025-06-09 11:03:41 -0500

    Include an IEmployee Interface

---
+++ C#/Programación con C# y .NET/Solid con C# y .NET/2-OpenClose/IEmployee.cs
@@ -0,0 +1,9 @@
+namespace OpenClose
+{
+    public interface IEmployee
+    {
+        public string Fullname { get; set; }
+        public int HoursWorked { get; set; }
+        public decimal CalculateSalaryMonthly();
+    }
+}
