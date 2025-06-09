e94dfe3df9b858bca8c4d53fabdfece579eafe45
Juan Galvis <Galvis>
2025-06-09 11:13:17 -0500

    Main program

---
+++ C#/Programación con C# y .NET/Solid con C# y .NET/2-OpenClose/Program.cs
@@ -0,0 +1,16 @@
+using OpenClose;
+
+ShowSalaryMonthly(new List<IEmployee>() {
+    new EmployeeFullTime("Pepito Pérez", 160),
+    new EmployeePartTime("Manuel Lopera", 180),
+    new EmployeeContractor("Natalie Sierra", 250)
+});
+
+
+void ShowSalaryMonthly(List<IEmployee> employees)
+{
+    foreach (var employee in employees)
+    {
+        Console.WriteLine($"Empleado: {employee.Fullname}, Pago: {employee.CalculateSalaryMonthly()} ");
+    }
+}
